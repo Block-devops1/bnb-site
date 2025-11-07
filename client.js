504595c15762c1809d19807f43123257f934b79e
Block-devops1 <ugochukwulambert156@gmail.com>
2025-11-07 08:22:33 +0100

    committed changes.

--- client.js
+++ client.js
@@ -3,7 +3,7 @@
 //--------------------------------------------------
 
 document.addEventListener('DOMContentLoaded',() => {
-    const downloadButtoon =
+    const downloadButton =
  document.getElementById('download-btn');
 
         //Simple  scroll animation when the CTA button is clicked 
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded',() => {
         downloadButtoon.addEventListener('click',() => {
             // Scroll smoothly to the download section
 
-document.getElementById('download').scrollIntoView({ behaviour:'smooth' });
+document.getElementById('download').scrollIntoView({ behavior:'smooth' });
 
               // you can add analytics tracking here later!
               console.log('Download button clicked! Scrolling to download section.');
